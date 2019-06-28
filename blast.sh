6b8659964004591e6646543e0b648a982c08ed4f
lucyzhangzhang <lzsj1995@gmail.com>
2019-06-28 17:08:31 -0400

    jank line break

--- blast.sh
+++ blast.sh
@@ -8,15 +8,14 @@ fi
 
 #Recognize .fasta, .fna, and .fa files
 function blast_genes () {
+
+    if $(ls $DIR/blast/*.out); then
+        rm $DIR/blast/*.out
+    fi
+
     for i in $(ls $2/housekeep/*.f*a* & ls  $2/phos/*.f*a); do
-
-        blastn -query $i -db $1/*.f*a -outfmt 6 -evalue 0.00001 \
-            -max_hsps 1 -max_target_seqs 1 -task "dc-megablast" | \
-            sort -nrk 4,4  >> $DIR/blast/dcMega.out
-
-        blastn -query $i -db $1/*.f*a -outfmt 6 -evalue 0.00001 \ 
-            -max_hsps 1 -max_target_seqs 1 | \
-            sort -nrk 4,4  >> $DIR/blast/blast.out
+        blastn -query $i -db $1/*.f*a -outfmt 6 -evalue 0.00001 -max_hsps 1 -max_target_seqs 7 -task "dc-megablast" | sort -nrk 4,4 | head -n 1 >> $DIR/blast/dcMega.out
+        blastn -query $i -db $1/*.f*a -outfmt 6 -evalue 0.00001 -max_hsps 1 -max_target_seqs 7 | sort -nrk 4,4 | head -n 1 >> $DIR/blast/blast.out
     done
 }
 
@@ -30,8 +29,7 @@ function makedb () {
 }
 
 while read spec; do
-    echo -e "=============\n====$spec====\n=============" | \
-        tee -a $DIR/blast/dcMega.out $DIR/blast/blast.out
+    echo -e "=============\n====$spec====\n=============" | tee -a $DIR/blast/dcMega.out $DIR/blast/blast.out
     DATA=$DIR/transcripts/$spec
     makedb $DATA
     blast_genes $DATA $DIR/genes
